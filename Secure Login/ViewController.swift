3fb9254c3f78cd0e9a6626c23998a4dcf0fa216c
navianme <navenvijay2011@gmail.com>
2019-06-18 00:23:24 -0400

    aes 218

--- Secure Login/ViewController.swift
+++ Secure Login/ViewController.swift
@@ -7,14 +7,38 @@
 //
 
 import UIKit
+import CryptoSwift
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var txtPassword: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        //save()
     }
 
+    @IBOutlet weak var textPassword: UITextField!
+    func save()
+    {
+        if let password = txtPassword.text
+        {
+            do {
+                let aes = try AES(key: "passwordpassword", iv: "drowssapdrowssap") // aes128
+                let ciphertext = try aes.encrypt(Array(password.utf8))
+                print("real password",password, "AES128 = ",ciphertext.toHexString())
+            } catch {
+                print(error)
+            }
 
+        //print("real password", password, "sha 256 = ", a, "****", a.count)
+        }
+    }
+    
+
+    
+    @IBAction func btnActionLogin(_ sender: Any) {
+    save()
+    }
 }
 
